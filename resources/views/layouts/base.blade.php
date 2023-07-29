2241e50baef10833b55bd210f3871d0162b97ad2
rizki <muhammadrizkitkj123@gmail.com>
2023-07-29 19:31:11 +0800

    disable splashscreen

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>SIAMUD</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
@@ -40,10 +41,10 @@
     <div class="wrapper">
 
         <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
+        {{-- <div class="preloader flex-column justify-content-center align-items-center">
             <img class="animation__shake" src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
                 height="60" width="60">
-        </div>
+        </div> --}}
 
         @include('layouts.navbar')
         @include('layouts.sidebar')

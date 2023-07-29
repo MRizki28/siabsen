145c1b3410ea55527c6bd3c9ab43c5697aa340a6
rizki <muhammadrizkitkj123@gmail.com>
2023-07-29 19:31:47 +0800

    change sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,32 +1,24 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #2d84f4 ">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">SIAMUD</span>
+    <a href="/" class="brand-link" style="border-bottom:3px solid white">
+        <img src="{{ asset('logo2.png') }}" alt="AdminLTE Logo"
+            class="brand-image" style="
+            width: 45px;
+            margin-top: 2px;
+            margin-left: 8px;
+        ">
+        <span class="brand-text font-weight-bold">SIAMUD</span>
     </a>
-
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
-                    alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Dimas</a>
-            </div>
-        </div>
-
         <!-- SidebarSearch Form -->
-        <div class="form-inline">
+        <div class="form-inline mt-3 ">
             <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                <input class="form-control form-control-sidebar bg-light border-0" type="search" placeholder="Search"
                     aria-label="Search">
                 <div class="input-group-append">
-                    <button class="btn btn-sidebar">
+                    <button class="btn btn-sidebar bg-light border-0">
                         <i class="fas fa-search fa-fw"></i>
                     </button>
                 </div>
@@ -38,29 +30,40 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
                 <li class="nav-item">
-                    <a href="/" class="nav-link">
+                    <a href="/" class="nav-link text-light">
                         <i class="nav-icon far fa-calendar-alt"></i>
                         <p>
                             Dashboard
                         </p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="/divisi" class="nav-link">
-                        <i class="nav-icon fas fa-building"></i>                
-                        <p>
-                            Divisi
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/absen" class="nav-link">
-                        <i class="nav-icon fas fa-pen"></i>                  
-                        <p>
-                            Absen
-                        </p>
-                    </a>
-                </li>
+                @if (auth()->user()->role == 1)
+                    <li class="nav-item">
+                        <a href="/divisi" class="nav-link text-light">
+                            <i class="nav-icon fas fa-building"></i>
+                            <p>
+                                Divisi
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/data/absen" class="nav-link text-light">
+                            <i class="nav-icon fas fa-pen"></i>
+                            <p>
+                                Data Absen
+                            </p>
+                        </a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a href="/absen" class="nav-link text-light">
+                            <i class="nav-icon fas fa-pen"></i>
+                            <p>
+                                Absen
+                            </p>
+                        </a>
+                    </li>
+                @endif
             </ul>
         </nav>
         <!-- /.sidebar-menu -->

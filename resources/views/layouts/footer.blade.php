d4049192e99e26d81baf157fa50cceb4620c72f6
rizki <muhammadrizkitkj123@gmail.com>
2023-07-29 19:31:22 +0800

    change footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,6 +2,6 @@
     <strong>Copyright &copy; 2023 <a href="#">Dimas</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 1.0.0
     </div>
   </footer>

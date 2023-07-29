a17c6bb414f63e8ce9aaf320cd5d47565c0dfb43
rizki <muhammadrizkitkj123@gmail.com>
2023-07-29 19:27:50 +0800

    create seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,5 +22,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create(array(
+            'id' => rand(100, 500),
+            'uuid' => 'e4ea84b9-237e-4d58-ba5c-181acbb1f381',
+            'name' => 'Admin Marmud',
+            'role' => 1,
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now()->toTimeString(),
+            'password' => Hash::make('admin123'),
+        ));
     }
 }
